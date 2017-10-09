8ae31b311be7888a28458d556544dda4afaa4bee
vonrepiks <ico_skipernov@abv.bg>
2017-10-09 19:41:47 +0300

    Modify port to work with heroku

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const port = 3000;
+const port = 3000 || process.env.PORT;
 const config = require('./config/config');
 const database = require('./config/database.config');
 const express = require('express');
 
 let app = express();
-let environment = process.env.NODE_enviroment || 'development';
+let environment = process.env.MONGOLAB_URI || 'development';
 
 database(config[environment]);
 require('./config/express')(app, config[environment]);
