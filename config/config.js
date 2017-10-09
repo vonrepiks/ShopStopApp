3cd557a8c5cb76daa906ec409a3914385b64a262
vonrepiks <ico_skipernov@abv.bg>
2017-10-09 22:29:00 +0300

    modify connection string to work with mlab

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     development: {
-        connectionString: 'mongodb://localhost:27017/ShopStopDatabase',
+        connectionString: 'mongodb://shop-stop-db:ico123@ds113925.mlab.com:13925/heroku_csc1hrvb',
         rootPath: path.normalize(path.join(__dirname, '../'))
     },
     production: {
