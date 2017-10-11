82c567e10dcaff4b6083b0d5a86e2135d9434406
vonrepiks <ico_skipernov@abv.bg>
2017-10-11 05:10:22 +0300

    Define using MongoClient option in mongoose connection and heroku port

--- config/database.config.js
+++ config/database.config.js
@@ -3,7 +3,9 @@ const utils = require('../utils');
 mongoose.Promise = global.Promise;
 
 module.exports = (config) => {
-    mongoose.connect(config.connectionString);
+    mongoose.connect(config.connectionString, {
+        useMongoClient: true
+    });
 
     let database = mongoose.connection;
     
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const port = 3000;
+const port = process.env.PORT || 3000;
 const config = require('./config/config');
 const database = require('./config/database.config');
 const express = require('express');
 
 let app = express();
-let environment = process.env.NODE_enviroment || 'development';
+let environment = 'development';
 
 database(config[environment]);
 require('./config/express')(app, config[environment]);
