c43e0642bfc58ea3b11c3b899c5724139b160ab8
vonrepiks <ico_skipernov@abv.bg>
2017-10-11 05:37:22 +0300

    update name of connection String to sync with heroku

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     development: {
-        connectionString: process.env.MONGOLAB_URI,
+        connectionString: process.env.PROD_MONGODB,
         rootPath: path.normalize(path.join(__dirname, '../'))
     },
     production: {
